60c0605168f2c50de767ba9af2d4ac6b0a4531b8
DELIA DUMITRU <delia.dumitru1@stud.ubbcluj.ro>
2023-11-25 08:36:50 +0200

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Dumitru_Delia_Lab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
